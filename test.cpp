49cbf86bc8f6b526764c2200bb523b8de64f99f8
Milan <mitka3@uniba.sk>
2014-12-21 16:06:41 +0100

    init

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+const double PI = 3.1415926535;
+char myGrade = 'A';
+bool isHappy = true;
+int myAge = 23;
+float favNum = 3.1415926535;
+double otherFav = 2.718281828;
+
+long long int trt = 94;
+
+int main(){
+  cout << "Hello world!" << endl;
+
+  cout << ((3 < 4) ? "ano" : "nie") << endl;
+
+  printf("hello\n");
+
+  return 0;
+}
