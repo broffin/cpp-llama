d96c591c786379a2b6a0b3358f9cff68d8e070b7
Milan <broffin@users.noreply.github.com>
2014-12-22 17:16:18 +0100

    add savings

---
+++ savings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+  double suma = 0;
+  int rok = 0;
+
+  cout << "Zakladám účet..." << endl;
+  cout << "Rok" << "\t" << "Suma" << endl;
+
+  while (suma < 20000) {
+    suma = (suma + 1000) * 1.05;
+    rok++;
+    fprintf(stdout, "%i \t %.2f\n", rok, suma);
+    //cout << rok << "\t" << suma << endl;
+  }
+
+  cout << "Trvalo to len " << rok << " rokov!";
+}
