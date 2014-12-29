e2fb7e5a0e45d6fbae36b2eb4cbf99ea777362a9
Milan <broffin@users.noreply.github.com>
2014-12-29 11:21:41 +0100

    use long to prevent integer overflow

--- sieve.cpp
+++ sieve.cpp
@@ -1,18 +1,19 @@
+// Print numbers < N not divisible by any j^2 where j > 1.
+
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  const int N = 250000;
+int main() {
+  const int N = 50;
   bool* A = new bool[N + 1];
 
   fill_n(A, N + 1, true);
-
   cout << 1 << " ";
-
-  for (int i = 2; i <= N; i++) {
+  
+  for (long i = 2; i <= N; i++) {
     if (A[i]) {
       cout << i << " ";
-      for (int j = i*i; j <= N; j += i*i) {
+      for (long j = i*i; j <= N; j += i*i) {
         A[j] = false;
       }
     }
